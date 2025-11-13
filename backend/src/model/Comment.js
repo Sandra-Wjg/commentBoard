a556a968073da0853a07a694fee4d11be6e96291
Sandra <19938170528@163.com>
2025-11-13 18:17:27 +0800

    model

---
+++ backend/src/model/Comment.js
@@ -0,0 +1,10 @@
+// Comment model
+const mogoose = require("../db/db");
+
+const CommentSchema = mogoose.Schema({
+  username: String,
+  content: { type: String, required: true },
+});
+const Comment = mogoose.model("comment", CommentSchema);
+
+module.exports = Comment;
---
+++ backend/src/model/User.js
@@ -0,0 +1,24 @@
+// User model
+const mogoose = require("../db/db");
+
+const UserSchema = mogoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: String,
+    age: Number,
+    city: String,
+    gender: {
+      type: Number,
+      default: 0, // 0-保密，1-男，2-女
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mogoose.model("user", UserSchema);
+
+module.exports = User;
